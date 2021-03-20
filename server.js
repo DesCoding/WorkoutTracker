42f299930da34089a17abf23aa04157902fc5b51
Des <d.stout31@yahoo.com>
2021-03-20 16:30:10 -0500

    updated mongoose connection for heroku

--- server.js
+++ server.js
@@ -10,20 +10,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  "mongodb+srv://DesCoding:DesAtlas17222@cluster0.o7lix.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  }
-);
-
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-//   useNewUrlParser: true,
-//   useFindAndModify: false,
-// });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
 // routes
 app.use(require("./routes/api.js"));
