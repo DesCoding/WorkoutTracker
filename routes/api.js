c1e50b0b9c38468e833132b4f9981993dc84c71e
Des <d.stout31@yahoo.com>
2021-03-20 16:21:34 -0500

    created heroku app name, created api get for duration and weight ranges with mongodb .limit

--- routes/api.js
+++ routes/api.js
@@ -23,9 +23,9 @@ router.post("/api/workouts", (req, res) => {
       res.status(400).json(err);
     });
 });
-//ask Anna about this
+
 router.put("/api/workouts/:id", (req, res) => {
-  Workouts.findByIdAndUpdate(req.params.id)
+  Workouts.findByIdAndUpdate(req.params.id, { $set: { exercises: req.body } })
     .then((dbWorkouts) => {
       console.log("workout updated");
       res.json(dbWorkouts);
@@ -35,38 +35,18 @@ router.put("/api/workouts/:id", (req, res) => {
     });
 });
 
-// const router = require("express").Router();
-// const Transaction = require("../models/transaction.js");
-
-// router.post("/api/transaction", ({ body }, res) => {
-//   Transaction.create(body)
-//     .then((dbTransaction) => {
-//       res.json(dbTransaction);
-//     })
-//     .catch((err) => {
-//       res.status(400).json(err);
-//     });
-// });
-
-// router.post("/api/transaction/bulk", ({ body }, res) => {
-//   Transaction.insertMany(body)
-//     .then((dbTransaction) => {
-//       res.json(dbTransaction);
-//     })
-//     .catch((err) => {
-//       res.status(400).json(err);
-//     });
-// });
-
-// router.get("/api/transaction", (req, res) => {
-//   Transaction.find({})
-//     .sort({ date: -1 })
-//     .then((dbTransaction) => {
-//       res.json(dbTransaction);
-//     })
-//     .catch((err) => {
-//       res.status(400).json(err);
-//     });
-// });
+router.get("/api/workouts/range", (req, res) => {
+  Workouts.aggregate([
+    { $addFields: { totalDuration: { $sum: "$exercises.duration" } } },
+  ])
+    .sort({ _id: -1 })
+    .limit(7)
+    .then((dbWorkouts) => {
+      res.json(dbWorkouts);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
 
 module.exports = router;
